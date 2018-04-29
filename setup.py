c0d95b3849c062619f90969df87940028a929465
Steffen Vogel <post@steffenvogel.de>
2018-04-29 18:13:58 +0200

    added setuptools script

---
+++ setup.py
@@ -0,0 +1,54 @@
+import os
+import re
+from setuptools import setup
+
+def cleanhtml(raw_html):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
+
+def read(fname):
+    dname = os.path.dirname(__file__)
+    fname = os.path.join(dname, fname)
+
+    with open(fname) as f:
+        contents = f.read()
+        sanatized = cleanhtml(contents)
+
+        try:
+            from m2r import M2R
+            m2r = M2R()
+            return m2r(sanatized)
+        except:
+            return sanatized
+
+setup(
+    name = "pushfin",
+    version = "0.1.0",
+    author = "Steffen Vogel",
+    author_email = "post@steffenvogel.de",
+    description = "Publishes bank account statements via MQTT and Pushover.",
+    license = "GPL-3.0",
+    keywords = "HBCI FinTS Pushover MQTT",
+    url = "https://github.com/stv0g/pushfin",
+    long_description = read('README.md'),
+    scripts=[
+	    'bin/pushfin'
+    ],
+    setup_requires = [
+        'm2r',
+    ],
+    install_requires = [
+	'pyyaml',
+	'fints'
+    ],
+    zip_safe = False,
+    classifiers = [
+	"Development Status :: 5 - Production/Stable",
+	"Programming Language :: Python :: 3.6",
+	"Programming Language :: Python :: 3.7",
+	"Topic :: Office/Business :: Financial",
+	"Topic :: Home Automation",
+	"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
+    ]
+)
